a789ac7701968fe3cb804d2ae4929a8668dd738b
Francis Emefile <emefilefranklin@gmail.com>
2018-04-22 20:07:07 +0100

    Added EvilCircle object and its behaviours. Implemented a ball counter.

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ function Ball(x, y, velX, velY, exists, color, size){
 Ball.prototype = Object.create(Shape.prototype);
 Ball.prototype.constructor = Ball;
 
+
 /*
 *method draw() draws a Ball on the canvas
 */
@@ -58,6 +59,10 @@ Ball.prototype.draw = function(){
   ctx.fill();
 };
 
+/*
+*method update() implements edge-collision-detection
+*and updates the this.x and this.y by adding this.velX and this.velY respectively
+*/
 Ball.prototype.update = function(){
   if((this.x + this.size) >= width){ //if Ball goes out of the screen on the right
     this.velX = -(this.velX); //bring it back by moving the ball left
@@ -95,8 +100,93 @@ Ball.prototype.collisionDetect = function() {
 
 };
 
-var balls = []; //to hold the Ball objects
+function EvilCircle(x, y, exists, color, size, velX, velY){
+  Shape.call(this, x, y, exists);
+  this.color = 'white';
+  this.size = 10;
+  this.velX = 20;
+  this.velY = 20;
+}
 
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+/*
+*method draw() draws the EvilCircle on the canvas
+*/
+EvilCircle.prototype.draw = function(){
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+/*
+*method checkBounds() checks whether the EvilCircle is going off the edge of the screen
+*edge-collision-detection mechanism
+*/
+EvilCircle.prototype.checkBounds = function(){
+  if((this.x + this.size) >= width){ //if EvilCircle goes out of the screen on the right
+    this.x = this.x - this.size; //bring it back by moving the EvilCircle left
+  }
+
+  if((this.x - this.size) <= 0) { //if EvilCircle goes out of the screen on the left
+    this.x = this.x + this.size; //bring it back by moving the EvilCircle right
+  }
+
+  if((this.y - this.size) <= 0) { //if EvilCircle goes out of the screen on the top
+    this.y = this.y + this.size; //bring it back by moving the EvilCircle downwards
+  }
+
+  if((this.y + this.size) >= height) { //if EvilCircle goes out of the screen at the bottom
+    this.y3 = this.y - this.size;  //bring it back by moving the EvilCircle upwards
+  }
+
+};
+
+/*
+*method setControls() implements the functionality that enables the
+*EvilCircle to be moved using the arrow keys
+*/
+EvilCircle.prototype.setControls = function() {
+  var _this = this;
+  window.onkeydown = function(e) {
+    if(e.keyCode === 37){ //right
+      _this.x -= _this.velX;
+    }else if(e.keyCode === 39){ //left
+      _this.x += _this.velX;
+    }else if(e.keyCode === 38){ // down
+      _this.y -= _this.velY;
+    }else if(e.keyCode === 40){ //up
+      _this.y += _this.velY;
+    }
+  };
+}
+
+EvilCircle.prototype.collisionDetect = function() {
+    for(var j = 0; j<balls.length; j++) {
+      if(balls[j].exists) { //check to see if the ball has not been destroyed
+        var dx = this.x - balls[j].x;
+        var dy = this.y - balls[j].y;
+        var distance = Math.sqrt(dx*dx + dy*dy); //distance between to points
+        if(distance < this.size + balls[j].size){
+          balls[j].exists = false;
+          ballCount--;
+          paragraph.textContent = 'Ball Count: ' + ballCount;
+        }
+      }
+    }
+
+};
+
+var balls = []; //to hold the Ball objects
+var ballCount = 0;
+
+var paragraph = document.querySelector('p');
+
+var evilCircle = new EvilCircle(random(0, width), random(0, height), true);
+evilCircle.setControls();
 /*
 * loop() covers the canvas with a semi-black rectangle each time it runs
 * it also create 25 Ball objects and stores them in the balls array
@@ -118,14 +208,23 @@ function loop() {
       random(10, 20)
     );
     balls.push(ball);
+    ballCount++;
+    paragraph.innerHTML = 'Ball count: ' + ballCount;
   }
 
   for(var i=0; i < balls.length; i++){
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if(balls[i].exists){
+      balls[i].draw();
+
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+
   }
 
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
   requestAnimationFrame(loop);
 }
 
